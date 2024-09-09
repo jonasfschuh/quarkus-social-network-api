ca18053116b28cd9f5d6323165065742cce351d3
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 16:00:22 -0300

    update db.sql

--- src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResources.java
+++ src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResources.java
@@ -1,5 +1,8 @@
 package io.github.jonasfschuh.quarlusSocialNetwork.rest;
 
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.User;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.UserRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -9,13 +12,30 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResources {
 
+    private final UserRepository repository;
+
+    @Inject
+    public PostResources(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @POST
-    public Response savePost() {
+    public Response savePost(@PathParam("userId") Long userId) {
+        User user = repository.findById(userId);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
         return Response.status(Response.Status.CREATED).build();
     }
 
     @GET
-    public Response listAllPosts() {
+    public Response listAllPosts(@PathParam("userId") Long userId) {
+        User user = repository.findById(userId);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
         return Response.ok().build();
     }
 
