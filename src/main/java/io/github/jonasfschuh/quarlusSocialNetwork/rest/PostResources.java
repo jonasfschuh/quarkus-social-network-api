d08ea859bd40f5c5d05ebec991ec96604908bf4e
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 16:10:08 -0300

    update PostResources

--- src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResources.java
+++ src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResources.java
@@ -1,8 +1,12 @@
 package io.github.jonasfschuh.quarlusSocialNetwork.rest;
 
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.Post;
 import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.User;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.PostRepository;
 import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.UserRepository;
+import io.github.jonasfschuh.quarlusSocialNetwork.rest.dto.CreatePostRequest;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -13,18 +17,27 @@ import jakarta.ws.rs.core.Response;
 public class PostResources {
 
     private final UserRepository repository;
+    private final PostRepository postRepository;
 
     @Inject
-    public PostResources(UserRepository repository) {
+    public PostResources(UserRepository repository,
+                         PostRepository postRepository) {
         this.repository = repository;
+        this.postRepository = postRepository;
     }
 
     @POST
-    public Response savePost(@PathParam("userId") Long userId) {
+    @Transactional
+    public Response savePost(@PathParam("userId") Long userId, CreatePostRequest postRequest) {
         User user = repository.findById(userId);
         if (user == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+
+        Post post = new Post();
+        post.setText(postRequest.getText());
+        post.setUser(user);
+        repository.persist(post);
 
         return Response.status(Response.Status.CREATED).build();
     }
