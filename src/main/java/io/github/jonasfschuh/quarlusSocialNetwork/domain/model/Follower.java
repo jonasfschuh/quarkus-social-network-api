c7cdbb312890ef80b74d6f13ddece4f3e76c6e1d
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 19:25:55 -0300

    adjust Followers.java

--- src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/domain/model/Follower.java
+++ src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/domain/model/Follower.java
@@ -2,21 +2,19 @@ package io.github.jonasfschuh.quarlusSocialNetwork.domain.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "followers")
 @Data
-public class Followers {
+public class Follower {
 
     @Id
-    @generatedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "follower_id")
     private User follower;
 
