6ce844cc7ae506cba609ddf33b19132b417d361c
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 13:53:21 -0300

    create ResponseError

---
+++ src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/dto/ResponseError.java
@@ -0,0 +1,45 @@
+package io.github.jonasfschuh.quarlusSocialNetwork.rest.dto;
+
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ResponseError {
+    private String message;
+    private Collection<FieldError> errors;
+
+    public static <T> ResponseError createFromValidation(
+            Set<ConstraintViolation<T>> violations) {
+        List<FieldError> errors = violations
+                .stream()
+                .map(v -> new FieldError(v.getPropertyPath().toString(), v.getMessage()))
+                .collect(Collectors.toList());
+        String message = "Validation Error";
+        var responseError = new ResponseError(message, errors);
+        return responseError;
+    }
+
+    public ResponseError(String message, Collection<FieldError> errors) {
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Collection<FieldError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Collection<FieldError> errors) {
+        this.errors = errors;
+    }
+}
