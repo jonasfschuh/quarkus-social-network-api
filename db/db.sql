0aa70698b19852769a4ee5104c9a61b9d830a6dd
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 10:18:50 -0300

    update db.sql file

--- db/db.sql
+++ db/db.sql
@@ -4,4 +4,5 @@ create table USERS (
                        id bigserial not null primary key,
                        name varchar(100) not null,
                        age integer not null
-)
+);
+
