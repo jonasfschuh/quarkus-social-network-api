73443a5cb3253e3bb00da0f64a14dfc28e874531
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-09 18:59:34 -0300

    created FollowerResources.java

---
+++ src/main/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/FollowerResources.java
@@ -0,0 +1,24 @@
+package io.github.jonasfschuh.quarlusSocialNetwork.rest;
+
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.FollowerRepository;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.UserRepository;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/users/{userId}/followers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces uces(MediaType.APPLICATION_JSON)
+public class FollowerResources {
+
+    private final FollowerRepository followerRepository;
+    private final UserRepository userRepository;
+
+    @Inject
+    public FollowerResources(FollowerRepository followerRepository, UserRepository userRepository) {
+        this.followerRepository = followerRepository;
+        this.userRepository = userRepository;
+    }
+}
