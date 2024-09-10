d1e6bb6e8fa6b275854db263af96667c40a4dc16
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-10 14:23:11 -0300

    updated PostResourceTest

--- src/test/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResourceTest.java
+++ src/test/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/PostResourceTest.java
@@ -1,6 +1,10 @@
 package io.github.jonasfschuh.quarlusSocialNetwork.rest;
 
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.Follower;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.Post;
 import io.github.jonasfschuh.quarlusSocialNetwork.domain.model.User;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.FollowerRepository;
+import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.PostRepository;
 import io.github.jonasfschuh.quarlusSocialNetwork.domain.repository.UserRepository;
 import io.github.jonasfschuh.quarlusSocialNetwork.rest.dto.CreatePostRequest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
@@ -8,6 +12,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,25 +27,59 @@ class PostResourceTest {
 
     @Inject
     UserRepository userRepository;
+    @Inject
+    FollowerRepository followerRepository;
+    @Inject
+    PostRepository postRepository;
+
     Long userId;
+    Long userNotFollowerId;
+    Long userFollowerId;
 
     @BeforeEach
     @Transactional
-    public void setup() {
+    public void setUP(){
+        //standard user test
         var user = new User();
-        user.setName("User Test");
         user.setAge(30);
+        user.setName("Fulano");
         userRepository.persist(user);
         userId = user.getId();
+
+        //post created for user
+        Post post = new Post();
+        post.setText("Hello");
+        post.setUser(user);
+        postRepository.persist(post);
+
+        //user doesn´t follower anyone
+        var userNotFollower = new User();
+        userNotFollower.setAge(33);
+        userNotFollower.setName("Cicrano");
+        userRepository.persist(userNotFollower);
+        userNotFollowerId = userNotFollower.getId();
+
+        //follower user
+        var userFollower = new User();
+        userFollower.setAge(31);
+        userFollower.setName("Terceiro");
+        userRepository.persist(userFollower);
+        userFollowerId = userFollower.getId();
+
+        Follower follower = new Follower();
+        follower.setUser(user);
+        follower.setFollower(userFollower);
+        followerRepository.persist(follower);
+
     }
 
     @Test
-    @DisplayName("Should create a post successfully")
-    public void createPostTest() {
+    @DisplayName("should create a post for a user")
+    public void createPostTest(){
         var postRequest = new CreatePostRequest();
-        postRequest.setText("Post Test");
+        postRequest.setText("Some text");
 
-       given()
+        given()
             .contentType(ContentType.JSON)
             .body(postRequest)
             .pathParam("userId", userId)
@@ -50,12 +90,12 @@ class PostResourceTest {
     }
 
     @Test
-    @DisplayName("Should return 404 when trying to make a post to a non-existent user")
-    public void inexistentUserTest() {
+    @DisplayName("should return 404 when trying to make a post for an inexistent user")
+    public void postForAnInexistentUserTest(){
         var postRequest = new CreatePostRequest();
-        postRequest.setText("Post Test");
+        postRequest.setText("Some text");
 
-        var inexistentUserId = 999L;
+        var inexistentUserId = 999;
 
         given()
             .contentType(ContentType.JSON)
@@ -68,12 +108,12 @@ class PostResourceTest {
     }
 
     @Test
-    @DisplayName("Should return 404 when user doesn´t exist")
-    public void listPostUserNotFoundTest() {
-        var inexistentUserId = 999L;
+    @DisplayName("should return 404 when user doesn't exist")
+    public void listPostUserNotFoundTest(){
+        var inexistentUserId = 999;
+
         given()
             .pathParam("userId", inexistentUserId)
-            .header("followerId", userId)
         .when()
             .get()
         .then()
@@ -81,52 +121,57 @@ class PostResourceTest {
     }
 
     @Test
-    @DisplayName("Should return 400 when followerId header is not present ")
-    public void listPostFollowerHeaderNotSendTest() {
+    @DisplayName("should return 400 when followerId header is not present")
+    public void listPostFollowerHeaderNotSendTest(){
         given()
             .pathParam("userId", userId)
         .when()
             .get()
         .then()
-            .statusCode(400);
+            .statusCode(400)
+            .body(Matchers.is("You forgot to header followerId!"));
     }
 
     @Test
-    @DisplayName("Should return 400 when follower doesn´t exist ")
-    public void listPostFollowerNotFoundTest() {
-        var inexistentFollowerId = 999L;
+    @DisplayName("should return 400 when follower doesn't exist")
+    public void listPostFollowerNotFoundTest(){
+
+        var inexistentFollowerId = 999;
+
         given()
             .pathParam("userId", userId)
             .header("followerId", inexistentFollowerId)
         .when()
             .get()
         .then()
-            .statusCode(400);
+            .statusCode(400)
+            .body(Matchers.is("Inexistent followerId!"));
     }
 
     @Test
-    @DisplayName("Should return 403 when follower isn´t a follower ")
-    public void listPostFollowerNotAFollowerTest() {
-        var followerId = userId;
+    @DisplayName("should return 403 when follower isn't a follower")
+    public void listPostNotAFollower(){
         given()
             .pathParam("userId", userId)
-            .header("followerId", followerId)
+            .header("followerId", userNotFollowerId)
         .when()
             .get()
         .then()
-            .statusCode(403);
+            .statusCode(403)
+            .body(Matchers.is("You can´t see these user´s posts!"));
     }
 
     @Test
-    @DisplayName("Should return posts")
-    public void listPostTest() {
-        var follower = new User();
-        follower.setName("Follower Test");
-        follower.setAge(30);
-        userRepository.persist(follower);
-
-
+    @DisplayName("should list posts")
+    public void listPostsTest(){
+        given()
+            .pathParam("userId", userId)
+            .header("followerId", userFollowerId)
+        .when()
+            .get()
+        .then()
+            .statusCode(Response.Status.FOUND.getStatusCode())
+            .body("size()", Matchers.is(1));
     }
-
 
 }
