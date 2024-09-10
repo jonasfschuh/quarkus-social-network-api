e48e52e540d4d36aba3d8b1d1006657c55b907aa
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-10 12:54:37 -0300

    created second Test Case

--- src/test/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/UserResourceTest.java
+++ src/test/java/io/github/jonasfschuh/quarlusSocialNetwork/rest/UserResourceTest.java
@@ -1,6 +1,7 @@
 package io.github.jonasfschuh.quarlusSocialNetwork.rest;
 
 import io.github.jonasfschuh.quarlusSocialNetwork.rest.dto.CreateUserRequest;
+import io.github.jonasfschuh.quarlusSocialNetwork.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.mapper.ObjectMapperType;
@@ -31,6 +32,26 @@ class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should return error when json is invalid")
+    public void createUserValidationErrorTest() {
+        var user = new CreateUserRequest();
+        user.setName(null);
+        user.setAge(null);
+
+        var response =
+            given()
+                .contentType(ContentType.JSON)
+                .body(user)
+            .when()
+                .post("/users")
+            .then()
+                .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
     }
 
 }
